089c3edf51018843d0c13004ffdbc4f52a078f5a
Kaan Uygur <kaanuygur@voltmac.local>
2017-05-03 01:00:26 +0300

    Added lamp logic

--- index.js
+++ index.js
@@ -12,38 +12,78 @@ admin.initializeApp({
 });
 
 var db = admin.database();
-var ref = db.ref("climate");
-// var ledRef = refLed.chield("humidity");
+var refBathroom = db.ref("lamp/bathroom");
+var refKitchen = db.ref("lamp/kitchen");
+var refSaloon = db.ref("lamp/saloon");
+//  var ledBathroomRef = refLed.chield("bathroom");
+// var ledKitchenRef = refLed.chield("kitchen");
+// var ledSaloonRef = refLed.chield("saloon");
 // ref.once("value", function(snapshot) {
 //   console.log(snapshot.val());
 // });
 
+// ref.once("value", function(snapshot) {
+//   console.log(snapshot.val());
+// }, function (errorObject) {
+//   console.log("The firebase.db read failed: " + errorObject.code);
+// });
 
-// var db = admin.database();
-// var ref = db.ref("server/saving-data/fireblog/posts");
+// refBathroom.on("child_changed", function (snapshot) {
+//   var changedPost = snapshot.val();
+//   if (changedPost == 1) {
 
-// // Attach an asynchronous callback to read the data at our posts reference
-ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-  console.log("The firebase.db read failed: " + errorObject.code);
-});
+//   }
+//   console.log("The updated post title is " + changedPost);
+// });
+
+
+
+
+
+
 
 var board = new five.Board({
   io: new Raspi()
 });
 
-board.on("ready", function() {
+board.on("ready", function () {
   var ledGreen = new five.Led("P1-11");
   var ledYellow = new five.Led("P1-13");
   var ledRed = new five.Led("P1-15");
 
-  ledGreen.blink();
-  if (ledGreen.on){
-    ledYellow.blink();
-    if (ledYellow.on) {
-      ledRed.blink();
-    }
-  }
 
+
+  refBathroom.on("child_changed", function (snapshot) {
+    var changedPost = snapshot.val();
+    if (changedPost == 1) {
+      ledGreen.on();
+    } else {
+      ledGreen.off();
+    }
+  });
+
+  refKitchen.on("child_changed", function (snapshot) {
+    var changedPost = snapshot.val();
+    if (changedPost == 1) {
+      ledYellow.on();
+    } else {
+      ledYellow.off();
+    }
+  });
+
+  refSaloon.on("child_changed", function (snapshot) {
+    var changedPost = snapshot.val();
+    if (changedPost == 1) {
+      ledRed.on();
+    } else {
+      ledRed.off();
+    }
+  });
+
+  // ledGreen.blink();
+  // if (ledGreen.on){
+  //   ledYellow.blink();
+  //   if (ledYellow.on) {
+  //     ledRed.blink();
+  //   }
 });
