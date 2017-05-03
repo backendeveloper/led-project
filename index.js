165ceb7878e586c294bdde31a8192217ab4c62f5
Kaan Uygur <kaanuygur@voltmac.local>
2017-05-03 06:52:43 +0300

    asdas

--- index.js
+++ index.js
@@ -50,137 +50,132 @@ board.on("ready", function () {
   var ledYellow = new five.Led("P1-13");
   var ledRed = new five.Led("P1-15");
   var piezo = new five.Piezo("P1-12");
-
-  var myObject = {
-    controller: "BME280",
-    lastName: "Doe",
-    fullName: function () {
-      this.fullName = 1000;
-      return this;
-    }
-  }
-  var multi = new five.Multi({
+  var multis = function () {
+    var _freq = 10000;
+    refFreq.on("child_changed", function (snapshot) {
+      _freq = snapshot.val();
+    });
+    var multi = new five.Multi({
       controller: "BME280",
-      freq: function () {
-         this.freq = 10000;
-        refFreq.on("child_changed", function (snapshot) {
-          this.freq = snapshot.val();
-        });
-        // this.freq = 10000;
-        return this.freq.freq;
-      }
+      freq: _freq
     });
-    // board.repl.inject({
-    //   piezo: piezo
-    // });
-    multi.on("data", function () {
-      console.log("Thermometer");
-      console.log("  celsius      : ", this.thermometer.celsius);
-      console.log("  fahrenheit   : ", this.thermometer.fahrenheit);
-      console.log("  kelvin       : ", this.thermometer.kelvin);
-      console.log("--------------------------------------");
+  }
 
-      console.log("Barometer");
-      console.log("  pressure     : ", this.barometer.pressure);
-      console.log("--------------------------------------");
+  // var multi = new five.Multi({
+  //   controller: "BME280",
+  //   freq: 1000
+  // });
+  // board.repl.inject({
+  //   piezo: piezo
+  // });
+  multis.call.multi.on("data", function () {
+    console.log("Thermometer");
+    console.log("  celsius      : ", this.thermometer.celsius);
+    console.log("  fahrenheit   : ", this.thermometer.fahrenheit);
+    console.log("  kelvin       : ", this.thermometer.kelvin);
+    console.log("--------------------------------------");
 
-      console.log("Hygrometer");
-      console.log("  humidity     : ", this.hygrometer.relativeHumidity);
-      console.log("--------------------------------------");
+    console.log("Barometer");
+    console.log("  pressure     : ", this.barometer.pressure);
+    console.log("--------------------------------------");
 
-      console.log("Altimeter");
-      console.log("  feet         : ", this.altimeter.feet);
-      console.log("  meters       : ", this.altimeter.meters);
-      console.log("--------------------------------------");
-    });
+    console.log("Hygrometer");
+    console.log("  humidity     : ", this.hygrometer.relativeHumidity);
+    console.log("--------------------------------------");
 
-    refAll.on("child_changed", function (snapshot) {
-      var changedPost = snapshot.val();
-      if (changedPost == 3) {
-        refBathroom.update({
-          "led": 1
-        });
-        refKitchen.update({
-          "led": 1
-        });
-        refSaloon.update({
-          "led": 1
-        });
-        maestro();
-      }
-      if (changedPost == 0) {
-        refBathroom.update({
-          "led": 0
-        });
-        refKitchen.update({
-          "led": 0
-        });
-        refSaloon.update({
-          "led": 0
-        });
-      }
-    });
+    console.log("Altimeter");
+    console.log("  feet         : ", this.altimeter.feet);
+    console.log("  meters       : ", this.altimeter.meters);
+    console.log("--------------------------------------");
+  });
 
-    refBathroom.on("child_changed", function (snapshot) {
-      var changedPost = snapshot.val();
-      if (changedPost == 1) {
-        allLedCounter(1);
-        ledGreen.on();
-      } else {
-        allLedCounter(-1);
-        ledGreen.off();
-      }
-    });
-
-    refKitchen.on("child_changed", function (snapshot) {
-      var changedPost = snapshot.val();
-      if (changedPost == 1) {
-        allLedCounter(1);
-        ledYellow.on();
-      } else {
-        allLedCounter(-1);
-        ledYellow.off();
-      }
-    });
-
-    refSaloon.on("child_changed", function (snapshot) {
-      var changedPost = snapshot.val();
-      if (changedPost == 1) {
-        allLedCounter(1);
-        ledRed.on();
-      } else {
-        allLedCounter(-1);
-        ledRed.off();
-      }
-    });
-
-    var allLedCounter = function (counter) {
-      if (allCounter == 0 && counter < 0)
-        return;
-
-      allCounter = allCounter + counter;
-      refAll.update({
-        "led": allCounter
+  refAll.on("child_changed", function (snapshot) {
+    var changedPost = snapshot.val();
+    if (changedPost == 3) {
+      refBathroom.update({
+        "led": 1
       });
-    };
-
-    var maestro = function () {
-      piezo.play({
-        // song is composed by a string of notes
-        // a default beat is set, and the default octave is used
-        // any invalid note is read as "no note"
-        song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -",
-        beats: 1 / 4,
-        tempo: 500
+      refKitchen.update({
+        "led": 1
       });
-    };
-
-    this.on("exit", function () {
-      refAll.update({
+      refSaloon.update({
+        "led": 1
+      });
+      maestro();
+    }
+    if (changedPost == 0) {
+      refBathroom.update({
         "led": 0
       });
+      refKitchen.update({
+        "led": 0
+      });
+      refSaloon.update({
+        "led": 0
+      });
+    }
+  });
+
+  refBathroom.on("child_changed", function (snapshot) {
+    var changedPost = snapshot.val();
+    if (changedPost == 1) {
+      allLedCounter(1);
+      ledGreen.on();
+    } else {
+      allLedCounter(-1);
+      ledGreen.off();
+    }
+  });
+
+  refKitchen.on("child_changed", function (snapshot) {
+    var changedPost = snapshot.val();
+    if (changedPost == 1) {
+      allLedCounter(1);
+      ledYellow.on();
+    } else {
+      allLedCounter(-1);
+      ledYellow.off();
+    }
+  });
+
+  refSaloon.on("child_changed", function (snapshot) {
+    var changedPost = snapshot.val();
+    if (changedPost == 1) {
+      allLedCounter(1);
+      ledRed.on();
+    } else {
+      allLedCounter(-1);
+      ledRed.off();
+    }
+  });
+
+  var allLedCounter = function (counter) {
+    if (allCounter == 0 && counter < 0)
+      return;
+
+    allCounter = allCounter + counter;
+    refAll.update({
+      "led": allCounter
+    });
+  };
+
+  var maestro = function () {
+    piezo.play({
+      // song is composed by a string of notes
+      // a default beat is set, and the default octave is used
+      // any invalid note is read as "no note"
+      song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -",
+      beats: 1 / 4,
+      tempo: 500
+    });
+  };
+
+  this.on("exit", function () {
+    refAll.update({
+      "led": 0
     });
   });
+});
 
 
 
