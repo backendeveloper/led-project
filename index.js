8fc3ff01ac9ea9aa138010015307b0fed41efb04
Kaan Uygur <kaanuygur@voltmac.local>
2017-05-05 01:21:23 +0300

    asdsd

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ board.on("ready", function () {
   var piezo = new five.Piezo("P1-40");
   var servo = new five.Servo({
     pin: "P1-12",
-    startAt: 120
+    startAt: 0
   });
   var multi = new five.Multi({
 
