d8c73629eb1023a26ad26aee779a40fb3fe3c711
Kaan Uygur <kaanuygur@voltmac.local>
2017-05-05 01:07:32 +0300

    sdas

--- index.js
+++ index.js
@@ -40,8 +40,7 @@ board.on("ready", function () {
   var animation = new five.Animation(servo);
 
   animation.enqueue({
-    center: true,
-    cuePoints: [0, 0.25],
+    cuePoints: [0, 0.25, 0.75, 1],
     keyFrames: [90, { value: 180, easing: "inQuad" }, { value: 0, easing: "outQuad" }, 90],
     duration: 1000
   });
