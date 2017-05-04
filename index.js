21bea276117063e675105be2f420988f3b7039f8
Kaan Uygur <kaanuygur@voltmac.local>
2017-05-05 00:47:25 +0300

    asdsad

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ var board = new five.Board({
 
 board.on("ready", function () {
   var ledGreen = new five.Led("P1-11");
-  var ledYellow = new five.Led("P1-13");
+  var ledYellow = new five.Led("P1-37");
   var ledRed = new five.Led("P1-15");
   var piezo = new five.Piezo("P1-12");
-  var servo = new five.Servo("P1-37");
+  var servo = new five.Servo("P1-13");
   var multi = new five.Multi({
     controller: "BME280",
     freq: 3000
